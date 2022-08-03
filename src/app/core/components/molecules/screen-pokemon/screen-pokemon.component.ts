dc0d554686384d6e12a831a13db99434d4f0d5fb
Manuel Galindo <mfdogalindo@outlook.com>
2022-08-02 22:23:10 -0500

    Pokemon image autoupdate

--- src/app/core/components/molecules/screen-pokemon/screen-pokemon.component.ts
+++ src/app/core/components/molecules/screen-pokemon/screen-pokemon.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
 import { Pokemon } from 'src/app/core/models';
 
 @Component({
@@ -6,11 +7,49 @@ import { Pokemon } from 'src/app/core/models';
   templateUrl: './screen-pokemon.component.html',
   styleUrls: ['./screen-pokemon.component.scss'],
 })
-export class ScreenPokemonComponent implements OnInit {
-  @Input() pokemon: Pokemon = {};
+export class ScreenPokemonComponent implements OnInit, OnDestroy {
   @Output() onClickBack = new EventEmitter<void>();
-  
+  @Input('pokemon')
+  public set pokemonSet(val: Pokemon) {
+    this.pokemon = val;
+    this.indexSprites();
+  }
+
+  pokemon: Pokemon = {};
+
+  pokemonSprite: { current: number; index: string[] } = {
+    current: 0,
+    index: [],
+  };
+
+  spriteChangerSub!: Subscription;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.setSpriteChanger();
+  }
+
+  ngOnDestroy(): void {
+    this.spriteChangerSub.unsubscribe();
+  }
+
+  indexSprites() {
+    let sprites = this.pokemon.sprites!!
+    let index : any[] = Object.entries(sprites).map(([key, value], index) => {
+      return value;
+    }).filter((value) => typeof(value) === 'string');
+    this.pokemonSprite.current = 1;
+    this.pokemonSprite.index = index;
+  }
+  
+  setSpriteChanger(){
+    this.spriteChangerSub = timer(0, 1000).subscribe(()=>{
+      this.changeSprite();
+    })
+  }
+
+  changeSprite() {
+    this.pokemonSprite.current = (this.pokemonSprite.current + 1) % this.pokemonSprite.index.length;
+  }
 }
